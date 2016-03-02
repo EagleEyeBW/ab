7973b76ce48460837ef4d322bd4edb9ab185e577
kay <kay.czarnotta@maxdome.de>
2016-03-02 14:20:18 +0100

    add test

---
+++ UnitTestProject1/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            throw new Exception("möööp");
+        }
+    }
+}
